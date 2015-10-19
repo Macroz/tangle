ecff48bbf44a3b073b5464588f00f3e2329b1661
Markku Rontu <markku.rontu@iki.fi>
2015-10-19 14:46:46 +0300

    Only render label for topmost graph, not subgraphs

--- src/tangle/core.clj
+++ src/tangle/core.clj
@@ -189,7 +189,7 @@
            (cond current-cluster (str "subgraph cluster_" (cluster->id current-cluster))
                  directed? "digraph"
                  :else "graph")
-           (if (get-in options [:graph :label])
+           (if (and (not current-cluster) (get-in options [:graph :label]))
              (str " \"" (escape (get-in options [:graph :label])) "\"")
              "")
            " {\n"
